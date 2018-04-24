8c125d4c45fd45b140a55c323db7a0b3b0eb95ee
deviltt <tt416926063@qq.com>
2018-04-24 15:16:04 +0800

    if(-1)的问题,返回值应该是0而不是-1

--- chapter3/3.17.c
+++ chapter3/3.17.c
@@ -54,47 +54,75 @@ int StackEmpty(SqStack S)
 	if(S.top == S.base)
 		return 1;
 	else
-		return -1;
+		return 0;
 }
 
-int Algo_3_17(char *s)
+void ClearStack_Sq(SqStack *S)
 {
-	SqStack S;
+	S->top = S->base;
+}
+
+void DestroyStack_Sq(SqStack *S)
+{
+	free(S->base);
+
+	S->top = NULL;
+	S->base = NULL;
+	S->stacksize = 0;
+}
+
+int Algo_3_17(SqStack *S, char *s)
+{
 	int e, i = 0;
 
-	InitStack(&S);
 
 	while(s[i] != '@' && s[i] != '&'){
-		Push_Sq(&S, s[i]);
+		Push_Sq(S, s[i]);
 		i++;
 	}
 
-	if(s[i] != '@'){
+	if(s[i] != '@' && s[i] == '&'){
 		i++;
-		while(!StackEmpty(S) && s[i] != '@'){
-			Pop_Sq(&S, &e);
-			if(e != s[i])
+		printf("i = %d\n", i);
+		printf("%c \n", s[i]);
+		while(!StackEmpty(*S) && s[i] != '@'){
+			Pop_Sq(S, &e);
+			if(e != s[i]){
+				printf("t");
 				return 0;
+			}
 			i++;
 		}
 	}
-
-	if(StackEmpty(S) && s[i] == '@')
+	printf("i = %d\n", i);
+	printf("%c \n", s[i]);
+	if(s[i] == '@'){
+		printf("tt");
 		return 1;
-	else
+	}
+	else{
+		printf("ttt");
 		return 0;
+	}
 }
 
 int main()
 {
-	char *s = "a+b-c&c-b+a@";
+	SqStack S;
+	char *s = "ab-c&c-b+a@";
+
+	InitStack(&S);
 
 	printf("判断s的序列是否满足要求\n");
 	
-	if(Algo_3_17(s))
+	if(Algo_3_17(&S, s))
 		printf("序列满足要求\n");
 	else
 		printf("序列不满足要求\n");
+
+	ClearStack_Sq(&S);
+
+	DestroyStack_Sq(&S);
 
 	return 0;
 }
